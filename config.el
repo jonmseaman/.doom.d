816d8508662c7df3655aba26f3b3767848340d7a
Jon Seaman <jon@pop-os.localdomain>
2022-10-14 18:14:40 -0400

    Add emacs config

---
+++ config.el
@@ -0,0 +1,165 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Jon Seaman"
+      user-mail-address "jonmseaman@live.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Workspace/Notes/Org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+
+
+;; Enable the GUI
+
+
+(menu-bar-mode 1)
+(scroll-bar-mode 1)
+;; (tool-bar-mode 1)
+;; (setq inhibit-splash-screen nil)
+;; (global-hl-line-mode nil)
+
+(after! org
+   (setq org-directory "C:/Users/jonms/Workspace/Notes/Org"
+      org-agenda-files '("C:/Users/jonms/Workspace/Notes/Org/Inbox.org"
+                         "C:/Users/jonms/Workspace/Notes/Org/Jonathan.org"
+                         "C:/Users/jonms/Workspace/Notes/Org/Actions.org"
+                         "C:/Users/jonms/Workspace/Notes/Org/Tickler.org"
+                         "C:/Users/jonms/Workspace/Notes/Org/Waiting.org")
+      org-default-notes-file (expand-file-name "Inbox.org" org-directory)
+      org-log-done 'time)
+
+    (setq org-capture-templates '(("t" "Todo [inbox]" entry
+                          (file+headline "C:/Users/jonms/Workspace/Notes/Org/Inbox.org" "Tasks")
+                          "* TODO %i%?")
+                         ("T" "Tickler" entry
+                          (file+headline "C:/Users/jonms/Workspace/Notes/Org/Tickler.org" "Tickler")
+                          "* %i%? \n %U")))
+
+    (setq org-refile-targets '(("C:/Users/jonms/Workspace/Notes/Org/Jonathan.org" :maxlevel . 3)
+                       ("C:/Users/jonms/Workspace/Notes/Org/LaterMaybe.org" :level . 1)
+                       ("C:/Users/jonms/Workspace/Notes/Org/Waiting.org" :level . 2)
+                       ("C:/Users/jonms/Workspace/Notes/Org/Actions.org" :level . 2)
+                       ("C:/Users/jonms/Workspace/Notes/Org/Archive.org" :maxlevel . 3)
+                       ("C:/Users/jonms/Workspace/Notes/Org/Tickler.org" :maxlevel . 2)))
+
+    (setq org-src-preserve-indentation nil
+      org-src-tab-acts-natively t
+      org-edit-src-content-indentation 0)
+
+    (with-no-warnings
+      (custom-declare-face '+org-todo-active  '((t (:inherit (bold font-lock-constant-face org-todo)))) "")
+      (custom-declare-face '+org-todo-project '((t (:inherit (bold font-lock-doc-face org-todo)))) "")
+      (custom-declare-face '+org-todo-onhold  '((t (:inherit (bold warning org-todo)))) "")
+      (custom-declare-face '+org-todo-kill  '((t (:inherit (error org-todo)))) ""))
+
+	(setq org-todo-keywords
+        '((sequence
+           "TODO(t)"  ; A task that needs doing & is ready to do
+           "PROJ(p)"  ; A project, which usually contains other tasks
+           "LOOP(r)"  ; A recurring task
+           "STRT(s)"  ; A task that is in progress
+           "WAIT(w)"  ; Something external is holding up this task
+           "HOLD(h)"  ; This task is paused/on hold because of me
+           "|"
+           "DONE(d)"  ; Task successfully completed
+           "KILL(k)") ; Task was cancelled, aborted or is no longer applicable
+          (sequence
+           "[ ](T)"   ; A task that needs doing
+           "[/](S)"   ; Task is in progress
+           "[?](W)"   ; Task is being held up or paused
+           "|"
+		   "[-](C)"   ; Task was cancelled.
+           "[X](D)")  ; Task was completed
+          (sequence
+           "|"
+           "OKAY(o)"
+           "YES(y)"
+           "NO(n)"))
+        org-todo-keyword-faces
+        '(("[-]"  . +org-todo-active)
+          ("STRT" . +org-todo-active)
+          ("[?]"  . +org-todo-onhold)
+          ("WAIT" . +org-todo-onhold)
+          ("HOLD" . +org-todo-onhold)
+          ("PROJ" . +org-todo-project)
+          ("NO"   . +org-todo-cancel)
+		  ))
+)
+
+
+(use-package! org-mouse
+  :custom
+  (org-mouse-features
+   '(context-menu move-tree yank-link activate-stars activate-bullets activate-checkboxes)))
+
+(setq scroll-step 1)
+(setq scroll-margin 7)
